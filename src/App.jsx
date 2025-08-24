b6e0331269c5c4b973159e7c3f9594f6577b6d22
Toni Fash <oluwatonifash@gmail.com>
2025-08-24 14:18:49 -0500

    verceltest

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ProductDetails from "./pages/ProductDetails";
-import ProductList from "./pages/productList";
-import CartList from "./pages/cartList";
+import ProductList from "./pages/ProductList";
+import CartList from "./pages/CartList";
 
 const App = () => {
   return (
