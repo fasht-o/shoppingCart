527a0b01b6cc01121757dce0cdf185f18ff7f3c2
Toni Fash <oluwatonifash@gmail.com>
2025-08-24 00:19:31 -0500

    product Details and first part of Cart

--- src/component/ProductItem.jsx
+++ src/component/ProductItem.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProductItem = ({ product }) => {
-  return <div>{product.title}</div>;
+  const navigate = useNavigate();
+  const handleNavigate = (productID) => {
+    console.log(productID);
+    navigate(`/product-details/${productID}`);
+  };
+  return (
+    <div className="relative group border border-cyan-700 cursor-pointer">
+      <div className="overflow-hidden aspect-w-1 aspect-h-1">
+        <img
+          src={product?.thumbnail}
+          alt={product?.title}
+          className="object-cover w-full h-full transiton-all duration-300 group-hover:scale-125"
+        />
+      </div>
+      <div className="flex items-start justify-between m-t4 space-x-4">
+        <div className="font-bold text-gray-900 sm:text-sm md:text-base">
+          <p className="w-[100px] overflow-hidden text-ellipsis whitespace-nowrap">
+            {product?.title}
+          </p>
+        </div>
+        <div className="text-right">
+          <p className="text-xs font-bold text-gray-900 sm:text-sm md:text-[14px]">
+            ${product.price}
+          </p>
+        </div>
+      </div>
+      <button
+        onClick={() => handleNavigate(product?.id)}
+        className="px-5 mt-5 w-full py-2 rounded-none bg-black text-white font-bold text-lg cursor-pointer"
+      >
+        View Details
+      </button>
+    </div>
+  );
 };
 
 export default ProductItem;
--- src/component/ShoppingContext.jsx
+++ src/component/ShoppingContext.jsx
@@ -1,8 +1,12 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 export const ShoppingCartContext = createContext(null);
 const ShoppingContext = ({ children }) => {
   const [loading, setLoading] = useState(true);
   const [productLists, setProductLists] = useState([]);
+  const [productDetails, setProductDetails] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const navigate = useNavigate();
   const fetchProductList = async () => {
     try {
       const resp = await fetch("https://dummyjson.com/products");
@@ -22,13 +26,45 @@ const ShoppingContext = ({ children }) => {
       setLoading(false);
     }
   };
+  const handleAddToCart = (productDetails) => {
+    console.log(productDetails);
+    let existingCartItems = [...cartItems];
+
+    const findIndexCart = existingCartItems.findIndex(
+      (cartItem) => cartItem.id === productDetails.id
+    );
+    console.log(findIndexCart);
+
+    if (findIndexCart === -1) {
+      existingCartItems.push({
+        ...productDetails,
+        quantity: 1,
+        totalPrice: productDetails?.price,
+      });
+    } else {
+    }
+    console.log(existingCartItems, "exist");
+    setCartItems(existingCartItems);
+    localStorage.setItem("cartItems", JSON.stringify(existingCartItems));
+    navigate("/cart");
+  };
   useEffect(() => {
     fetchProductList();
   }, []);
+  console.log(cartItems);
 
   return (
     <div>
-      <ShoppingCartContext.Provider value={{ productLists, loading }}>
+      <ShoppingCartContext.Provider
+        value={{
+          productLists,
+          loading,
+          productDetails,
+          setProductDetails,
+          handleAddToCart,
+          cartItems,
+        }}
+      >
         {children}
       </ShoppingCartContext.Provider>
     </div>
